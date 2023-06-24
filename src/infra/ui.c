3c51dec228efd97337f05b69833944b3e9accf72
AssoDePicche <samuelprado730@gmail.com>
2023-06-24 13:39:21 -0300

    feat: implement ui header

---
+++ src/infra/ui.c
@@ -0,0 +1,19 @@
+#include "../ui/ui.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_line()
+{
+  printf("----------\n");
+}
+
+void clear_screen()
+{
+  system("cls");
+}
+
+void pause()
+{
+  system("pause");
+}
+
